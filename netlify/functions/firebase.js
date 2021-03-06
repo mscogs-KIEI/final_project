05fe5908e5dacb900752d7642f6672dee15a75a0
Dan Peacock <dan.peacock@kellogg.northwestern.edu>
2021-03-06 11:24:33 -0600

    Firebase config

--- netlify/functions/firebase.js
+++ netlify/functions/firebase.js
@@ -1,7 +1,14 @@
 const firebase = require("firebase/app")
 require("firebase/firestore")
 
-const firebaseConfig = {} // replace
+const firebaseConfig = {
+  apiKey: "AIzaSyBReWli-h7d02S5RZ_kQ29sGKsUCwV5scQ",
+  authDomain: "kiei-451-dap.firebaseapp.com",
+  projectId: "kiei-451-dap",
+  storageBucket: "kiei-451-dap.appspot.com",
+  messagingSenderId: "800524210099",
+  appId: "1:800524210099:web:0f897514dc6a2b20a8c5c6"
+} // replace
 
 if (!firebase.apps.length) {
   firebase.initializeApp(firebaseConfig)
