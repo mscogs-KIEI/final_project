1761619f2ebe8f6e6cf77d151a9d11ef2db1115f
Dan Peacock <dan.peacock@kellogg.northwestern.edu>
2021-03-07 13:38:20 -0600

    checkIntoBar updates user doc in Firebase

--- index.js
+++ index.js
@@ -2,33 +2,57 @@ firebase.auth().onAuthStateChanged(async function(user) {
   if (user) {
     // Signed in
     console.log('signed in')
-    document.querySelector("#check-in").insertAdjacentHTML('beforeend', `
-      <div id='ci-FsTWmvFr4q9zOff1Ntjh' class='bg-green-500 hover:bg-green-600 text-white px-4 py-2 rounded-xl'>Check into Test Bar</div>
-    `)
-    document.querySelector("#check-in").addEventListener('click', async function(event) {
-      event.preventDefault()
-      console.log('Test check-in clicked')
-      let checkInResponse = await fetch('/.netlify/functions/checkIntoBar', {
-        method: 'POST',
-        body: JSON.stringify({
-          userId: user.uid,
-          bar: 'FsTWmvFr4q9zOff1Ntjh'
+    
+    // This block draws a bar-specific check-in button
+    // Bars are hard-coded for now, need to convert as part
+    // of the "lsit bars" story
+    let barList = [
+      {
+        Id: 'FsTWmvFr4q9zOff1Ntjh',
+        name: 'DryHop Brewers',
+        imageUrl: 'https://images.squarespace-cdn.com/content/v1/5bce90a8523958e26e9a434d/1600798629477-J2WQ5Y2ZBR0I3RJYUL5I/ke17ZwdGBToddI8pDm48kBzQH4RWPYqW2DD4NX3pz-JZw-zPPgdn4jUwVcJE1ZvWQUxwkmyExglNqGp0IvTJZamWLI2zvYWH8K3-s_4yszcp2ryTI0HqTOaaUohrI8PI517V6XQux86P6g_UYsh_OSCLQfLRlx_kfEHVvADs56A/DryHop+Brewers_Barman%27s+Banquet.png',
+      },
+      {
+        Id: 'dWNK0uAGeNySVL6gVs2H',
+        name: 'Prairie Moon',
+        imageUrl: 'https://dailynorthwestern.com/wp-content/uploads/2018/09/PrairieMoon_AllieGoulding_WEB-900x600.jpg'
+      }
+    ]
+    for (i = 0; i < barList.length; i++) {
+      // Define bar in question
+      let barId = barList[i].Id
+      let barName = barList[i].name
+      // Insert button
+      document.querySelector("#check-in").insertAdjacentHTML('beforeend', `
+        <div id='ci-${barId}' class='bg-green-500 hover:bg-green-600 text-white px-4 py-2 my-2 rounded-xl'>
+          Check in to ${barName}
+        </div>
+      `)
+      // Click event and POST to checkIntoBar
+      document.querySelector(`#ci-${barId}`).addEventListener('click', async function(event) {
+        event.preventDefault()
+        console.log(`${barName} check-in clicked`)
+        let checkInResponse = await fetch('/.netlify/functions/checkIntoBar', {
+          method: 'POST',
+          body: JSON.stringify({
+            userId: user.uid,
+            bar: barId
+          })
         })
+        let checkInData = await checkInResponse.json()
+        console.log(checkInData)
       })
-      let checkInData = await checkInResponse.json()
-      console.log(checkInData)
-      
-    })
-    document.querySelector("#ci-FsTWmvFr4q9zOff1Ntjh").addEventListener('mouseover', async function(event) {
-      event.preventDefault()
-      let mouseElement = document.querySelector("#ci-FsTWmvFr4q9zOff1Ntjh")
-      mouseElement.classList.add('cursor-pointer')
-    })
-    document.querySelector("#ci-FsTWmvFr4q9zOff1Ntjh").addEventListener('mouseout', async function(event) {
-      event.preventDefault()
-      let mouseElement = document.querySelector("#ci-FsTWmvFr4q9zOff1Ntjh")
-      mouseElement.classList.remove('cursor-pointer')
-    })
+
+      // These two blocks change the cursor to a hand on mouseover.
+      document.querySelector(`#ci-${barId}`).addEventListener('mouseover', async function(event) {
+        event.preventDefault()
+        event.target.classList.add('cursor-pointer')
+      })
+      document.querySelector(`#ci-${barId}`).addEventListener('mouseout', async function(event) {
+        event.preventDefault()
+        event.target.classList.remove('cursor-pointer')
+      })
+    }
   } else {
     // Signed out
     console.log('signed out')
--- netlify/functions/checkIntoBar.js
+++ netlify/functions/checkIntoBar.js
@@ -5,7 +5,11 @@ exports.handler = async function (event) {
     let db = firebase.firestore()
     let checkIn = JSON.parse(event.body)
     console.log(`Checking user ${checkIn.userId} into bar ${checkIn.bar}...`)
-    let checkInMessage = {message: 'Check in received'}
+    await db.collection('users').doc(`${checkIn.userId}`).update({
+        location: checkIn.bar,
+        lastCheckIn: firebase.firestore.FieldValue.serverTimestamp()
+    })
+    let checkInMessage = {message: `Checked in to bar ${checkIn.bar}`}
     return {
         statusCode: 200,
         body: JSON.stringify(checkInMessage)
