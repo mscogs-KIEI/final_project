fa14ee85e0606eb739537869f232bfe04f2dc41c
Dan Peacock <dan.peacock@kellogg.northwestern.edu>
2021-03-08 13:36:20 -0600

    Added some notes, made fetch work more reliably

--- index.js
+++ index.js
@@ -14,7 +14,35 @@ firebase.auth().onAuthStateChanged(async function(user) {
 
   if (user) {
     // Signed in
-    
+    console.log('signed in')
+    // "Going home" button to check self out for the night
+    document.querySelector('#check-in').insertAdjacentHTML('beforeend', `
+    <div id='going-home' class='button bg-blue-600 hover:bg-blue-800 text-white px-4 py-2 my-2 rounded-xl'>
+      I'm going home!
+    </div>
+    `)
+    document.querySelector('#going-home').addEventListener('click', async function(event) {
+      event.preventDefault()
+      console.log(`User ${user.uid} is going home for the night!`)
+      let checkOutResponse = await fetch('/.netlify/functions/checkSelfOut', {
+        method: 'POST',
+        body: JSON.stringify({
+          userId: user.uid
+        })
+      })
+      let checkOutData = await checkOutResponse.json()
+      console.log(checkOutData)
+    })
+    // These two blocks change the cursor to a hand on mouseover.
+    document.querySelector(`#going-home`).addEventListener('mouseover', async function(event) {
+      event.preventDefault()
+      event.target.classList.add('cursor-pointer')
+    })
+    document.querySelector(`#going-home`).addEventListener('mouseout', async function(event) {
+      event.preventDefault()
+      event.target.classList.remove('cursor-pointer')
+    })
+    // End of Going Home button.
 
     db.collection('users').doc(user.uid).set({
       name: user.displayName,
@@ -58,7 +86,10 @@ firebase.auth().onAuthStateChanged(async function(user) {
 
     // let bars = querySnapshot.docs
 
-    let response = await fetch(`http://localhost:55621/.netlify/functions/createNewBar`) //?userId=${user.uid} <-- add to link?
+    let response = await fetch(`/.netlify/functions/createNewBar`) //?userId=${user.uid} <-- add to link?
+          // Instead of '?userId=...', I think we need to use 'method: "POST"', like lines 12-17 of
+          // todo.js in the Week 9 solution [https://github.com/kiei451-winter2021/todos-final/blob/master/todo.js]
+          //  -Dan
     let bars = await response.json()
 
     for (let i=0; i<bars.length; i++) {
@@ -95,55 +126,25 @@ firebase.auth().onAuthStateChanged(async function(user) {
       firebase.auth().signOut()
       document.location.href = 'index.html'
     })
-
-    console.log('signed in')
-    
-    // "Going home" button to check self out for the night
-    document.querySelector('#check-in').insertAdjacentHTML('beforeend', `
-    <div id='going-home' class='button bg-blue-600 hover:bg-blue-800 text-white px-4 py-2 my-2 rounded-xl'>
-      I'm going home!
-    </div>
-    `)
-    document.querySelector('#going-home').addEventListener('click', async function(event) {
-      event.preventDefault()
-      console.log(`User ${user.uid} is going home for the night!`)
-      let checkOutResponse = await fetch('/.netlify/functions/checkSelfOut', {
-        method: 'POST',
-        body: JSON.stringify({
-          userId: user.uid
-        })
-      })
-      let checkOutData = await checkOutResponse.json()
-      console.log(checkOutData)
-    })
-    // These two blocks change the cursor to a hand on mouseover.
-    document.querySelector(`#going-home`).addEventListener('mouseover', async function(event) {
-      event.preventDefault()
-      event.target.classList.add('cursor-pointer')
-    })
-    document.querySelector(`#going-home`).addEventListener('mouseout', async function(event) {
-      event.preventDefault()
-      event.target.classList.remove('cursor-pointer')
-    })
     
     // This block draws bar-specific check-in buttons
     // Bars are hard-coded for now, need to convert as part
     // of the "list bars" story and merge into one loop
     let barList = [
       {
-        Id: 'FsTWmvFr4q9zOff1Ntjh',
+        id: 'FsTWmvFr4q9zOff1Ntjh',
         name: 'DryHop Brewers',
         imageUrl: 'https://images.squarespace-cdn.com/content/v1/5bce90a8523958e26e9a434d/1600798629477-J2WQ5Y2ZBR0I3RJYUL5I/ke17ZwdGBToddI8pDm48kBzQH4RWPYqW2DD4NX3pz-JZw-zPPgdn4jUwVcJE1ZvWQUxwkmyExglNqGp0IvTJZamWLI2zvYWH8K3-s_4yszcp2ryTI0HqTOaaUohrI8PI517V6XQux86P6g_UYsh_OSCLQfLRlx_kfEHVvADs56A/DryHop+Brewers_Barman%27s+Banquet.png',
       },
       {
-        Id: 'dWNK0uAGeNySVL6gVs2H',
+        id: 'dWNK0uAGeNySVL6gVs2H',
         name: 'Prairie Moon',
         imageUrl: 'https://dailynorthwestern.com/wp-content/uploads/2018/09/PrairieMoon_AllieGoulding_WEB-900x600.jpg'
       }
     ]
     for (let i = 0; i < barList.length; i++) {
       // Define bar in question
-      let barId = barList[i].Id
+      let barId = barList[i].id
       let barName = barList[i].name
       // Insert button
       document.querySelector("#check-in").insertAdjacentHTML('beforeend', `
@@ -165,7 +166,6 @@ firebase.auth().onAuthStateChanged(async function(user) {
         let checkInData = await checkInResponse.json()
         console.log(checkInData)
       })
-
       // These two blocks change the cursor to a hand on mouseover.
       document.querySelector(`#ci-${barId}`).addEventListener('mouseover', async function(event) {
         event.preventDefault()
@@ -175,6 +175,7 @@ firebase.auth().onAuthStateChanged(async function(user) {
         event.preventDefault()
         event.target.classList.remove('cursor-pointer')
       })
+      // End of check-in buttons
     }
   } else {
     // Not logged-in
